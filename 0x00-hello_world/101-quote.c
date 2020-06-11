5fa05553b7988269312f0e915af7935b9647fc2f
hurtadojara <1616@holbertonschool.com>
2020-06-10 23:17:54 -0500

    final fixes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,8 @@
 */
 int main(void)
 {
-	char fr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+char fr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", fr);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", fr);
+return (1);
 }
