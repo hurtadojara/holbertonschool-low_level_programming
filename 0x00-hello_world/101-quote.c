2629c42b6703cf0c2a2ec6b906d66b0a254d218e
hurtadojara <1616@holbertonschool.com>
2020-06-10 23:07:37 -0500

    final fixes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <unistd.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	char phrase[]  = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char fr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof(phrase));
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", fr);
 	return (1);
 }
