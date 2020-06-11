d649f9c6a4609e5e7f0e3a706695a075b0a38c6a
hurtadojara <1616@holbertonschool.com>
2020-06-11 15:00:11 -0500

    point 3 alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int character;
+
+	for (character = 'a'; character <= 'z'; character++)
+	{
+		putchar(character);
+	}
+	for (character = 'A'; character <= 'Z'; character++)
+	{
+	putchar(character);
+	}
+	putchar('\n');
+	return (0);
+}
