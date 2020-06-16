a907abcdcb76132ab06b00d7f28cb0d9ba795c7f
hurtadojara <1616@holbertonschool.com>
2020-06-16 13:02:42 -0500

    point 2 alphabet x1o

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - print alphabet in lowecase ten times
+ */
+void print_alphabet_x10(void)
+{
+	int counter, times;
+
+	for (times = 0; times <= 10; times++)
+	{
+	for (counter = 'a'; counter <= 'z'; counter++)
+	{
+		_putchar(counter);
+	}
+	_putchar(10);
+	}
+}
