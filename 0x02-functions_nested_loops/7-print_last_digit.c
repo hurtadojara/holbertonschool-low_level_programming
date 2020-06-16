868c1c4434b3c4faae3ec6b15d88959eac7c175f
hurtadojara <1616@holbertonschool.com>
2020-06-16 17:44:19 -0500

    checker issue

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_last_digit - does what name says
+ * Return: last number
+ * @n: takes number given
+ */
+int print_last_digit(int n)
+{
+	int digit;
+
+	if (n < 0)
+	{
+		n = n * -1;
+	}
+	if (n > 9 || n < -9)
+	{
+		digit = n % 10;
+		_putchar(digit + '0');
+		return (digit);
+	}
+	else if (n > -1 && n < 10)
+	{
+		_putchar(n + '0');
+		return (n);
+	}
+}
