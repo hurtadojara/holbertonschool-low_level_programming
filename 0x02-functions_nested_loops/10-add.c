81d3db43bfb9809849ceaa4f589c7a82c2f27026
hurtadojara <1616@holbertonschool.com>
2020-06-16 22:27:44 -0500

    adding point ajsjajs

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - does what name says
+ * Return: result
+ * @a: takes number given
+ * @b: takes number given
+ */
+int add(int a, int b)
+{
+	int valor;
+
+	valor = a + b;
+	return (valor);
+}
