a2f0a58644357de146c67fe2a32936c7ba5685ea
hurtadojara <1616@holbertonschool.com>
2020-06-18 16:21:34 -0500

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,12 +9,12 @@ int main(void)
 
 	for (number = 1; number < 101; number++)
 	{
-		if (number % 3 == 0)
+		if (number % 3 == 0 && number % 5 == 0)
+			printf("FizzBuzz ");
+		else if (number % 3 == 0 && number % 5 != 0)
 			printf("Fizz ");
 		else if (number % 5 == 0)
 			printf("Buzz ");
-		else if (number % 3 == 0 && number % 5 == 0)
-			printf("FizzBuzz ");
 		else
 			printf("%d ", number);
 	}
