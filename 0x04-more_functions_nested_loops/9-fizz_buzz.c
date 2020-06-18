56b8be97818dd7806d24b22a460b52f9d5be0923
hurtadojara <1616@holbertonschool.com>
2020-06-18 15:53:04 -0500

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - number from 1 to 100 fizzbuzz
+ */
+void main(void)
+{
+	int number;
+
+	for (number = 1; number < 101; number++)
+	{
+		if (number % 3 == 0)
+			printf("Fizz ");
+		else if (number % 5 == 0)
+			printf("Buzz ");
+		else if (number % 3 == 0 && number % 5 == 0)
+			printf("FizzBuzz ");
+		else
+			printf("%d ", number);
+	}
+	printf("\n");
+}
+
