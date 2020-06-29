579f04d257db5ca4be80d8a8278fcd807fad4ac2
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-06-29 15:14:36 -0500

    point 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,30 +1,30 @@
 #include "holberton.h"
 /**
- * 
- * 
- * 
- * 
+ *
+ *
+ *
+ *
  */
 char *_strstr(char *haystack, char *needle)
 {
-    int i, j, k;
-    char array[12], *p;
+	int i, j, k;
+	char array[12], *p;
 
-    for (j = 0; needle[j] != '\0'; j++)
-    {
-    }
-    for (i = 0; haystack[i] != '\0'; i++)
-    {
-        if (needle[0] == haystack[i])
-        {
-            for (k = 0; k < j; k++, i++)
-            {
-                array[k] = haystack[i];
-            }
-            array[k] = '\0'; 
-            p = array;
-            return (p);
-        }
-    }
-    return ('\0');
+	for (j = 0; needle[j] != '\0'; j++)
+	{
+	}
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (needle[0] == haystack[i])
+		{
+			for (k = 0; k < j; k++, i++)
+			{
+				array[k] = haystack[i];
+			}
+			array[k] = '\0';
+			p = array;
+			return (p);
+		}
+	}
+	return ('\0');
 }
