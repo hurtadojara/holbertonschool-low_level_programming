e8701540b8005bc609aba6ee0d5d8dcb192fbcea
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-07-01 20:17:30 -0500

    idk how to without loops

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _sqrt_recursion - square root
+ * @n: base
+ * Return: square
+ */
+int _sqrt_recursion(int n)
+{
+	if ((n * n) % n == 0)
+	{
+
+		return (0);
+	}
+}
