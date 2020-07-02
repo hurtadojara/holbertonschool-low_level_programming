cf62e5a066d7eec89c4d263bcd513313bb711151
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-07-01 21:10:51 -0500

    diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -6,5 +6,21 @@
  */
 void print_diagsums(int *a, int size)
 {
-	return (void);
+	int i, d1, diago1, n = size * size;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i % (size + 1) == 0)
+		{
+			d1 += a[i];
+		}
+	}
+	for (i = n - size; i > 0; i--)
+	{
+		if (i % (size - 1) == 0)
+		{
+			diago1 += a[i];
+		}
+		printf("%d, %d\n", d1, diago1);
+	}
 }
