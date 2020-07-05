2c5fe6ce08291aeac28bdf65ea40d937152545b0
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-07-05 17:03:20 -0500

    point 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - counters the args
+ * Return: 0 on success
+ * @argc: argument counter
+ * @argv: argument vector
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
