f251cfb7f3c90991b2c7e53c41467b839c02043c
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-07-05 19:28:42 -0500

    point 3 fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * 
- * 
- * 
- * 
+ * main - prints multiply
+ * Return: 1 on error 0 on success
+ * @argc: arguments counter
+ * @argv: arguments vector
  */
 int main(int argc, char *argv[])
 {
 	int result = 0, atoi1, atoi2;
 	char *str1 = argv[1], *str2 = argv[2];
 
-	if (argc)
+	if (argc != 2)
+	{
+		printf("Error");
+		return (1);
+	}
+	else
 	{
 	atoi1 = atoi(str1);
 	atoi2 = atoi(str2);
