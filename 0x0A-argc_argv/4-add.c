cb865034206146ecf4c375e39a47f53f6ead52d9
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-07-05 21:05:12 -0500

    point 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - sums integers
+ * Return: 0 un success 1 on failuree
+ * @argc: number of arguments
+ * @argv: arguments vector
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0, i;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] < '0' || *argv[i] > '9')
+		{
+			printf("Error\n");
+			return (1);
+		}
+		else
+		{
+			*argv[i] = atoi(argv[i]);
+			result += *argv[i];
+		}
+	}
+	printf("%d\n", result);
+	return (0);
+}
