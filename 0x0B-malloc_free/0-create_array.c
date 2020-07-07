200cd24f358c7c95b060088a2abeacaeb69822a6
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-07-07 15:20:00 -0500

    point 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * create_array - creates an array
+ * Return: return adress of the array
+ * @size: size of malloc
+ * @c: char
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptrbfr;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	if
+	{
+		ptrbfr = malloc(sizeof(char) * size);
+			if (ptrbfr == NULL)
+				return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		ptrbfr[i] = c;
+	}
+	return (ptrbfr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+__malloc and free__
