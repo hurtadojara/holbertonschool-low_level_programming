f882154bb66116bacb655b49fabddec1254e355e
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-07-09 15:51:40 -0500

    fix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,6 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		{
 			ptr[k + l] = s2[l];
 		}
+		ptr[k + l] = '\0';
 	}
 	else
 		{
@@ -40,6 +41,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		{
 			ptr[k + l] = s2[l];
 		}
+		ptr[k + l] = '\0';
 	}
 	return (ptr);
 }
