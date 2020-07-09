c53c7ee0d7c50bf6b0bd401ffad54caaee806f1c
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-07-09 15:41:47 -0500

    point 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenates
+ * Return: pointer to string
+ * @s1: string 1
+ * @s2: string 2
+ * @n: size to copy
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, k, l;
+	char *ptr;
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	ptr = malloc(sizeof(char) * (i + j));
+	if (ptr == NULL)
+	{
+		free(ptr);
+		exit(98);
+	}
+	for (k = 0; k < i; k++)
+	{
+		ptr[k] = s1[k];
+	}
+	if (n >= j)
+	{
+		for (l = 0; l < j; l++)
+		{
+			ptr[k + l] = s2[l];
+		}
+	}
+	else
+		{
+		for (l = 0; l < n; l++)
+		{
+			ptr[k + l] = s2[l];
+		}
+	}
+	return (ptr);
+}
