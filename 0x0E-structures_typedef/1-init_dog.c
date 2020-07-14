6aea02fa6a9ca8efc7da08acff5b62e136eda63c
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-07-13 21:52:39 -0500

    point 0 to 2

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,8 @@
+#include "dog.h"
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->age = age;
+	d->name = name;
+	d->owner = owner;
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,9 @@
+#ifndef DOGGIE
+#define DOGGIE
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+#endif
