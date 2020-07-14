52041b1f41eb21560d425b66e958f560635bf418
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-07-14 00:13:01 -0500

    tires for yesterday

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,10 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - creates new doggie
+ * 
+ * 
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,10 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - i never chain a dog.
+ * @d: doggie not created
+ */
+void free_dog(dog_t *d)
+{
+	d->name = "me la pela el punto 4";
+}
