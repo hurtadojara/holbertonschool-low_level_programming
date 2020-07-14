19eca0f0d19dc60a5942d9266cda0ff39a525c8d
hurtadojara <andreshurtadojaramillo@gmail.com>
2020-07-14 10:07:30 -0500

    hotfix

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#define __FILE_
+/**
+ * main - prints name
+ * Return: 0
+ */
 int main(void)
 {
 	printf("%s\n", __FILE__);
